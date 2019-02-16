353ef5119f22cfdfc44393fa2547b810c1283fd5
Hardik Pithva <hardikpthv@gmail.com>
2019-02-16 12:33:41 +0530

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import Dashboard from "./pages/dashboard";
 
 const myApp = "Welcome to CG Beer!";
 
 class App extends Component {
   render() {
-    return <div>{myApp}</div>;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Redirect from="/" exact to="/dashboard" />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
