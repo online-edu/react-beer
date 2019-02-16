34202f9802b2f4490ada621e3cbbfd6da229eae5
Hardik Pithva <hardikpthv@gmail.com>
2019-02-16 12:20:08 +0530

    feat: add basic react setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const myApp = "Welcome to CG Beer!";
+
+class App extends Component {
+  render() {
+    return <div>{myApp}</div>;
+  }
+}
+
+export default App;
