2b4b41c7deebcea3277e6b24b3561a81590213f1
Hardik Pithva <hardikpthv@gmail.com>
2019-02-16 20:03:51 +0530

    fix: routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-import Dashboard from "./pages/dashboard";
-
-const myApp = "Welcome to CG Beer!";
+import { HeaderBar } from "./components";
+import Beers from "./pages/Beers";
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Redirect from="/" exact to="/dashboard" />
-          <Route path="/dashboard" component={Dashboard} />
-        </Switch>
-      </BrowserRouter>
+      <div>
+        <HeaderBar />
+        <div className="cg-container my-4 mx-4">
+          <BrowserRouter>
+            <Switch>
+              {/* <Redirect from="/" exact to="/dashboard" /> */}
+              <Route path="/" component={Beers} />
+              <Route path="/dashboard" component={Beers} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </div>
     );
   }
 }
