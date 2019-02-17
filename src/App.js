bd2a544256b116aa272c2259b5fae5403ffd7939
Hardik Pithva <hardikpthv@gmail.com>
2019-02-17 19:24:36 +0530

    fix: lazy load detail component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component, Suspense, lazy } from "react";
 import { BrowserRouter, Switch, Route, withRouter } from "react-router-dom";
 import { HeaderBar, Spinner, Footer } from "./components";
 import Beers from "./pages/beers/Beers";
-// import BeerDetails from "./pages/beers/BeerDetails";
 import "./App.scss";
 
 const BeerDetails = withRouter(
